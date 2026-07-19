45cbf9efe91c3e87bc04efbd0a7edec8fd0243a7
Mega Yu <yuhg2310@gmail.com>
2026-07-19 12:15:41 +0800

    fix: embed Windows common controls manifest

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,12 @@
 fn main() {
-    tauri_build::build()
+    tauri_build::build();
+
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
+    let target_env = std::env::var("CARGO_CFG_TARGET_ENV").unwrap_or_default();
+
+    if target_os == "windows" && target_env == "msvc" {
+        let dependency = "/MANIFESTDEPENDENCY:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'";
+
+        println!("cargo:rustc-link-arg={dependency}");
+    }
 }
