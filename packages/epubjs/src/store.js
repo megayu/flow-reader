4a31ef9dadedf292e7f1cc2d3760c23bce67a610
Mega Yu <yuhg2310@gmail.com>
2026-07-29 19:07:58 +0800

    fix: initialize EPUB offline storage

--- packages/epubjs/src/store.js
+++ packages/epubjs/src/store.js
@@ -38,7 +38,7 @@ class Store {
   checkRequirements() {
     try {
       let store
-      if (typeof localforage === 'undefined') {
+      if (typeof localforage !== 'undefined') {
         store = localforage
       }
       this.storage = store.createInstance({
